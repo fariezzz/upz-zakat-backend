c26c3b762779980721fbb2bbd8bfd586717a00f3
Fariez <cihilxd46@gmail.com>
2026-08-13 20:56:25 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -16,12 +16,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        // Tambahkan domain produksi di sini nanti
-        // 'https://upz-unsil.ac.id',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
